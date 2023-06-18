338a3b0173e96ebf4465cd7c1849ec4941219204
nguyenlam <sweetloveinyourheart@gmail.com>
2023-06-18 09:19:11 +0700

    prepare for production deployment

--- sweet-social-server/src/main.ts
+++ sweet-social-server/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.setGlobalPrefix('v1/api');
   app.enableCors({})
   app.useGlobalPipes(new ValidationPipe())
 
